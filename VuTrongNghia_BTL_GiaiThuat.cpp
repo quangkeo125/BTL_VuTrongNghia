0a74d714d885a45fd503953705c4ca996b6f6fc8
Ngô Minh Quang <76208637+quangkeo125@users.noreply.github.com>
2024-03-19 01:28:34 +0700

    Delete NghiaDepTrai

--- VuTrongNghia_BTL_GiaiThuat.cpp
+++ VuTrongNghia_BTL_GiaiThuat.cpp
@@ -671,7 +671,6 @@ int main()
 						cout<<"\t 0.Thoat"<<endl;
 						cout<<"\t chon:";
 						cin>>choose;
-						cout<<"NghiaDepTrai";
 						switch(choose)
 						{
 							case 1: nhapdspt(PT); break;
